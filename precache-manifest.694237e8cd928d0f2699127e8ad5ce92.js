dcb063c1303f096690bb093d2e798222cbbe9f52
joao <jpdrc368@gmail.com>
2025-08-11 13:37:54 -0300

    Updates

---
+++ precache-manifest.694237e8cd928d0f2699127e8ad5ce92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10224c35ba94db9be357e0ad34d75bb8",
+    "url": "/Rede-de-colaboracao-PESC-2025/index.html"
+  },
+  {
+    "revision": "967de8fb9355dd18bff9",
+    "url": "/Rede-de-colaboracao-PESC-2025/static/css/main.831c0a5c.chunk.css"
+  },
+  {
+    "revision": "ba6ff7b239950f06ada7",
+    "url": "/Rede-de-colaboracao-PESC-2025/static/js/2.c4b9afbf.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Rede-de-colaboracao-PESC-2025/static/js/2.c4b9afbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967de8fb9355dd18bff9",
+    "url": "/Rede-de-colaboracao-PESC-2025/static/js/main.80bc5472.chunk.js"
+  },
+  {
+    "revision": "532f2e28704336475f0c",
+    "url": "/Rede-de-colaboracao-PESC-2025/static/js/runtime-main.d7b6f527.js"
+  }
+]);
